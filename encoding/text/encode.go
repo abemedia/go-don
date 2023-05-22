78065f65cbe835d739b305899d5548c8cc302d6b
Adam Bouqdib <adam@abemedia.co.uk>
2023-05-23 00:00:38 +0100

    refactor(encoding/text): reduce indentation

--- encoding/text/encode.go
+++ encoding/text/encode.go
@@ -12,52 +12,54 @@ import (
 
 //nolint:cyclop,funlen
 func encode(ctx *fasthttp.RequestCtx, v any) error {
+	if v == nil {
+		return nil
+	}
+
 	var (
 		b   []byte
 		err error
 	)
 
-	if v != nil {
-		switch v := v.(type) {
-		case string:
-			b = byteconv.Atob(v)
-		case []byte:
-			b = v
-		case int:
-			b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
-		case int8:
-			b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
-		case int16:
-			b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
-		case int32:
-			b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
-		case int64:
-			b = strconv.AppendInt(ctx.Response.Body(), v, 10)
-		case uint:
-			b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
-		case uint8:
-			b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
-		case uint16:
-			b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
-		case uint32:
-			b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
-		case uint64:
-			b = strconv.AppendUint(ctx.Response.Body(), v, 10)
-		case float32:
-			b = strconv.AppendFloat(ctx.Response.Body(), float64(v), 'f', -1, 32)
-		case float64:
-			b = strconv.AppendFloat(ctx.Response.Body(), v, 'f', -1, 64)
-		case bool:
-			b = strconv.AppendBool(ctx.Response.Body(), v)
-		case encoding.TextMarshaler:
-			b, err = v.MarshalText()
-		case error:
-			b = byteconv.Atob(v.Error())
-		case fmt.Stringer:
-			b = byteconv.Atob(v.String())
-		default:
-			return don.ErrNotAcceptable
-		}
+	switch v := v.(type) {
+	case string:
+		b = byteconv.Atob(v)
+	case []byte:
+		b = v
+	case int:
+		b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
+	case int8:
+		b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
+	case int16:
+		b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
+	case int32:
+		b = strconv.AppendInt(ctx.Response.Body(), int64(v), 10)
+	case int64:
+		b = strconv.AppendInt(ctx.Response.Body(), v, 10)
+	case uint:
+		b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
+	case uint8:
+		b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
+	case uint16:
+		b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
+	case uint32:
+		b = strconv.AppendUint(ctx.Response.Body(), uint64(v), 10)
+	case uint64:
+		b = strconv.AppendUint(ctx.Response.Body(), v, 10)
+	case float32:
+		b = strconv.AppendFloat(ctx.Response.Body(), float64(v), 'f', -1, 32)
+	case float64:
+		b = strconv.AppendFloat(ctx.Response.Body(), v, 'f', -1, 64)
+	case bool:
+		b = strconv.AppendBool(ctx.Response.Body(), v)
+	case encoding.TextMarshaler:
+		b, err = v.MarshalText()
+	case error:
+		b = byteconv.Atob(v.Error())
+	case fmt.Stringer:
+		b = byteconv.Atob(v.String())
+	default:
+		return don.ErrNotAcceptable
 	}
 
 	if err != nil {
