25a250e7916161175d9cd3fa88867e4efc50d2fe
Adam Bouqdib <adam@abemedia.co.uk>
2026-03-10 18:52:15 +0000

    chore: remove unnecessary parenthesis (#286)

--- encoding/text/text.go
+++ encoding/text/text.go
@@ -1,9 +1,7 @@
 // Package text provides encoding and decoding of plain text data.
 package text
 
-import (
-	"github.com/abemedia/go-don/encoding"
-)
+import "github.com/abemedia/go-don/encoding"
 
 func init() {
 	mediaType := "text/plain"
